5bb3fc600007aece8687e0da3be849ff950ec7b0
iceking2100 <iceking2100@gmail.com>
2025-05-27 19:42:10 -0400

    Created the Initial Scripts
    - Made the GameManager Persistent between scenes.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager gameManager { get; private set}
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
